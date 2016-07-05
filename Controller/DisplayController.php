135b511c1b7736d3c539385fa2a8c81678d50fcd
michael barbey <mbarbey@users.noreply.github.com>
2016-07-05 12:33:41 +0200

    Fix typo

--- Controller/DisplayController.php
+++ Controller/DisplayController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class DisplayController extends Controller
 {
 /**
      *
